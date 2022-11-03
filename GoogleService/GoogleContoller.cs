6e555dbce738dc10bbe480eaccbf72969c434054
Dragonliver <castanley77@live.com>
2022-11-03 10:48:29 -0600

    Making google api to help the front end

---
+++ GoogleService/GoogleContoller.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("google")]
+    public class Controller : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("getPlace")]
+        public Task<ActionResult> GetByPlace()
+        {
+            var key = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
+        }
+    }
+}
